48f8600437492830577ea9b3ecd7987fc600c02d
littleflute <13699175164@sina.cn>
2017-10-27 13:42:10 +0800

    Update xdui.js

--- xdui.js
+++ xdui.js
@@ -30,6 +30,18 @@ function blDiv(oBoss,id,html){
 var blTools = blBtn(document.body,"BLTOOLS","BlTools+"); 
 blTools._myJob = function(oBoss)
 {
+	function _Img(oBoss,id,src){
+    	var r = document.getElementById(id);
+    	if(!r){
+        	r = document.createElement("img");
+        	r.id = id;
+            r.style.position = "relative";  
+        	r.style.border = "1px solid #fd3d3d";   
+           	r.src = src; 
+    		if(oBoss!=null) oBoss.appendChild(r);
+    	}
+    	return r;
+	}
 	function _Div(oBoss,id,html){
     	var r = document.getElementById(id);
     	if(!r){
@@ -62,6 +74,8 @@ blTools._myJob = function(oBoss)
 	var j1 = _Div(oBoss,"j1","j1");
 	var j2 = _Div(oBoss,"j2","j2");
 	var j1b1 = _Btn(j2,"j1b1","j1b1");  
+	var j3 = _Div(oBoss,"j3","j3");
+    	var j3i1 = _Img(j3,"j3i1",  "https://user-images.githubusercontent.com/17950965/32089280-9b1df7f8-bb1b-11e7-973f-90ed3a1d9c67.jpg");
 }
 blTools.onclick = function(){
 	var divBlView = blDiv(document.body,"BLVIEW","BlView_V0.0.4");
@@ -81,6 +95,8 @@ var btnRun = document.getElementById("run");
 btnRun.onclick = function(){
 	blTools.onclick();
 }
+
+//v0.0.6 	+j3i1,_Img
 //v0.0.5
 //v0.0.4
 //v0.0.3
